d7784c40f86f64013db8f95645f01c64f8323d08
nicolaschia <nicolaschia9900@gmail.com>
2023-04-24 23:47:47 -0300

    correcciones en pages-css-javascript página terminada, proyecto aprobado :)

--- js/script.js
+++ js/script.js
@@ -35,3 +35,23 @@ nextBtn.addEventListener("click", function() {
 homeBtn.addEventListener("click", function() {
   window.location.href = "peticion.html"; //redirigir a la página 
 });
+
+
+//tu reseña
+var contador;
+function calificar(item){
+  console.log(item)
+  contador = item.id[0];
+  let nombre = item.id.substring(1);
+  for(let i=0;i<5;i++){
+    if(i<contador){
+      document.getElementById((i+1)+nombre).style.color=("orange");
+    }else{
+      document.getElementById((i+1)+nombre).style.color=("black");
+    }
+  }
+}
+
+function Enviar(){
+  alert("GRACIAS POR COMPARTIR TU RESEÑA Y CALIFICAR CON " +contador+" ESTRELLAS")
+}
